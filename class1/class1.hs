1a10b08528792059c81e6e16bba5602f6fbdf5a7
Axel Friberg <axl.friberg@gmail.com>
2015-11-16 09:56:31 +0100

    Updated class1.

--- class1/class1.hs
+++ class1/class1.hs
@@ -8,3 +8,21 @@ maxi x y =
 	if x > y
 	then x
 	else y
+
+-- sumsqr returns 1*1+2*2...+n*n
+sumsqr 0 = 0
+sumsqr n = n^2 + sumsqr (n-1)
+
+sumsqm n = sum (map (^2) [1..n])
+
+-- hanoi
+hanoi 0 = 0
+hanoi n = 1 + 2 * hanoi (n-1)
+
+-- Factors
+smallestFactor n = nextFactor 1 n
+
+nextFactor k n 
+  | k >= n            = n
+  | mod n (k+1) == 0  = k+1
+  | otherwise         = nextFactor (k+1) n
