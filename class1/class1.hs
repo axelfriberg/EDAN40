4ab3f3692debdbeb79e9dc7f117b61760015368a
Axel Friberg <axl.friberg@gmail.com>
2015-11-16 12:27:54 +0100

    Continued work on class 1.

--- class1/class1.hs
+++ class1/class1.hs
@@ -26,3 +26,30 @@ nextFactor k n
   | k >= n            = n
   | mod n (k+1) == 0  = k+1
   | otherwise         = nextFactor (k+1) n
+
+-- Why does't this funtion work?
+-- numFactors :: Int -> Int
+-- numFactors n =
+	--length . filter 0 (map (mod n) [1..n])
+
+-- Defining types
+type Month = Integer
+daysInMonth :: Month -> Integer -> Integer
+daysInMonth m y -- Why no equals here?
+  | m <= 0 || m > 12   = 0
+  | m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12  = 31
+  | m == 4 || m == 6 || m == 9 || m == 11   = 30
+  | mod y 4 == 0  = 29
+  | otherwise     = 28
+
+data Date = Date Integer Month Integer -- Why do you need to write Date first after equals?
+validDate :: Date -> Bool
+validDate (Date d m y)
+  | (d > 0) && (d <= daysInMonth m y) = True
+  | otherwise                         = False 
+
+-- Multiplying list elements
+multiply :: Num a => [a] -> a
+multiply [a] =
+	| length [a] == 0 = 0
+	| otherwise 
